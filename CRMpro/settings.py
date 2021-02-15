ea59aaaaccfd7a17849127f6c01045da1cb0d1bb
amanpaliwal777 <amanpaliwal777@gmail.com>
2021-02-16 00:17:58 +0530

    dfa

--- CRMpro/settings.py
+++ CRMpro/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 django_heroku.settings(locals())
