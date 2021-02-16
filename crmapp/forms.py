d14b324c2497d1f2e2260fbe2dd447f93929f773
amanpaliwal777 <amanpaliwal777@gmail.com>
2021-02-16 10:49:46 +0530

    vvv

--- crmapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-
-class UserCreate(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username']
